201dca6a2be8b8ac68899229141ef262f63a3cbe
Yousof Wakili <ywakili18@gmail.com>
2021-08-10 17:34:53 -0700

    Added feature to store player 1/2 information based on what player 1 selects first

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-let turn = false
-//-- POKEMON DATA
-//--TYPHLOSION
-const typhlosion = {
-  name: 'Typhlosion',
-  healthPoints: 500,
-  //nested array with object containing both Pokemon move name and damage data.
-  moveSet: {
-    flamethrower: 80,
-    eruption: 100
-  }
+//GLOBAL VARIABLES
+let playerOneTurn = true
+const pokemons = ['Typhlosion', 'Charizard']
+//OBJECT STORING PLAYER CHOICE
+playersChoice = {
+  player1: null,
+  player2: null
 }
-//--CHARIZARD
-const charizard = {
-  name: 'charizard',
-  healthPoints: 510,
-  //Array containing object with Pokemon move name and damage data.
-  moveSet: {
-    'Dragon Claw': 80,
-    'Blast Burn': 100
+//-- POKEMON DATA
+//--TYPHLOSION and CHARIZARD OBJECT DATA
+const pokemonObject = {
+  Typhlosion: {
+    healthPoints: 500,
+    //nested array with object containing both Pokemon move name and damage data.
+    moveSet: {
+      flamethrower: 80,
+      eruption: 100
+    }
+  },
+  Charizard: {
+    healthPoints: 510,
+    //Array containing object with Pokemon move name and damage data.
+    moveSet: {
+      'Dragon Claw': 80,
+      'Blast Burn': 100
+    }
   }
 }
 
@@ -25,8 +31,10 @@ const charizard = {
 const startButton = document.getElementById('startButton')
 //function that will promt user to make game choice
 const gameStart = function () {
+  //selecting pTag containing pokemon
   let buttonP = document.getElementById('startP')
   buttonP.innerText = 'Game Start! Player 1: Pick your pokemon below'
+  // event listener to show pokemon choices
   const selectTyph = document.getElementById('typh')
   selectTyph.classList.remove('hiddenPoke')
   const selectChar = document.getElementById('char')
@@ -35,18 +43,24 @@ const gameStart = function () {
 //event listener
 startButton.addEventListener('click', gameStart)
 
-//function containing event listner that will display HP value when user selects pokemon
-//typhlosion
-function pokeChoice(element, move, poke) {
-  element.innerText = `You choose ${poke.name} 
-  HP: ${poke.healthPoints}`
+// FUNCTION THAT TAKES CHOICE THAT USER PICKS AND DISPLAYS INFORMATION RELATED TO POKEMON CHOICE
+function pokeChoice(element, move, pokeName) {
+  // ASSIGNED POKE TO POKEMON OBJECT LISTED EARLIER,
+  // SELECTING POKEMON NAME AND DISPLAYING NAME + HEALTH POINTS
+  const poke = pokemonObject[pokeName]
 
-  // const selectTyphDiv = document.getElementById('typh')
-  // const showTyphHP = function () {
-  //   let typhClick = document.getElementById('displayHP1')
-  // }
-  // const x = selectTyphDiv.addEventListener('click', showTyphHP)
+  //FOR LOOP FOR POKEMON ARRAY LISTED EARLIER TO ASSIGN OPPOSITE POKEMON TO PLAYER 2
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i]
+    const pokemonID = document.getElementById(`${pokemon}ID`)
+    if (pokeName !== pokemon) {
+      playersChoice.player2 = pokemonObject[pokemon]
+      pokemonID.innerText = `Player 2 chooses: ${pokemon}`
+    } else {
+      playersChoice.player1 = poke
+      pokemonID.innerText = `Player 1 chooses: ${pokemon}`
+    }
+  }
 }
-pokeChoice()
-
-//make poke choices above to disappear
+//FUNCTION TO START BATTLE SEQUENCE
+const gameSequence = () => {}
