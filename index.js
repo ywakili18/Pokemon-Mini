611c8cf1887d1a9189c4ab9cae02b3e3cef38397
Yousof Wakili <ywakili18@gmail.com>
2021-11-26 15:22:36 -0800

    added sprites, restart button without reloading page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let player1 = ''
 let computer = ''
 
+let selectPoke = Array.from(document.getElementsByClassName('btn'))
+
 class Pokemon {
   constructor(name, hp, type, move) {
     ;(this.name = name), (this.hp = hp), (this.type = type), (this.move = move)
@@ -9,51 +11,68 @@ class Pokemon {
 const Cyndaquil = new Pokemon('Cyndaquil', 100, 'Fire', 'Ember')
 const Totodile = new Pokemon('Totodile', 100, 'Water', 'Water Gun')
 const Chikorita = new Pokemon('Chikorita', 100, 'Grass', 'Vine Whip')
-
 const names = [Cyndaquil.name, Totodile.name, Chikorita.name]
 const randomSelect = Math.floor(Math.random() * names.length)
 
-let selectPoke = Array.from(document.getElementsByClassName('btn'))
-let removeContainer = Array.from(document.getElementsByClassName('container'))
+const pokeSelect = () => {
+  selectPoke.map((pokeButton) => {
+    pokeButton.addEventListener('click', function (e) {
+      switch (e.target.value) {
+        case 'Cyndaquil':
+          player1 = e.target.value
+          computer = names[randomSelect]
+          removePoke()
+          startGame()
+          break
+        case 'Totodile':
+          player1 = e.target.value
+          computer = names[randomSelect]
+          removePoke()
+          startGame()
+          break
+        case 'Chikorita':
+          player1 = e.target.value
+          computer = names[randomSelect]
+          removePoke()
+          startGame()
+
+          break
+        default:
+          break
+      }
+    })
+  })
+}
+pokeSelect()
 // remove poke selection after game choice
 const removePoke = () => {
   selectPoke.map((poke) => {
     poke.remove()
   })
-  removeContainer.map((item) => {
-    item.remove()
-  })
+  document.getElementById('container').style.visibility = 'hidden'
 }
-const pokeSelect = () => {
-  selectPoke.map((pokeButton) => {
-    pokeButton.addEventListener(
-      'click',
-      function (e) {
-        switch (e.target.value) {
-          case 'Cyndaquil':
-            player1 = e.target.value
-            computer = names[randomSelect]
-            console.log(player1, computer)
-            removePoke()
-            break
-          case 'Totodile':
-            player1 = e.target.value
-            computer = names[randomSelect]
-            console.log(player1, computer)
-            removePoke()
-            break
-          case 'Chikorita':
-            player1 = e.target.value
-            computer = names[randomSelect]
-            console.log(player1, computer)
-            removePoke()
-            break
-          default:
-            break
-        }
-      },
-      { once: true }
-    )
-  })
+const startGame = () => {
+  let playerId = document.getElementById('player1')
+  let computerId = document.getElementById('computer')
+  playerId.innerHTML = `Player 1:`
+  computerId.innerHTML = `Computer:`
+  displayPokeData()
 }
-pokeSelect()
+// display pokemon data when user selects
+const displayPokeData = () => {
+  const playerDiv = document.getElementById(player1)
+  const playerClone = playerDiv.cloneNode(true)
+
+  const compDiv = document.getElementById(computer)
+  const compClone = compDiv.cloneNode(true)
+
+  const playerAndCompDiv = document.createElement('div')
+  playerAndCompDiv.appendChild(playerClone)
+  playerAndCompDiv.appendChild(compClone)
+  playerAndCompDiv.id = 'someId'
+  document.body.appendChild(playerAndCompDiv)
+}
+// restart game button
+function restartGame() {
+  window.location.reload(false)
+}
