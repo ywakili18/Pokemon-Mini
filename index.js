28f657f8b75811e23f55fed3ff776dc5b9d57bf5
Yousof Wakili <ywakili18@gmail.com>
2021-11-26 21:31:49 -0800

    added attack button functionality

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let player1 = ''
 let computer = ''
 // creates a iterable array from instead of HTML document and node.
 let selectPoke = Array.from(document.getElementsByClassName('btn'))
-
 // create class for pokemon that
 class Pokemon {
   constructor(name, hp, type, move) {
@@ -68,18 +67,69 @@ const startGame = () => {
 }
 // display pokemon data when user selects
 const displayPokeData = () => {
+  // grabbing current player and computer divs, cloning to replicate once user has chosen pokemon
   const playerDiv = document.getElementById(player1)
   const playerClone = playerDiv.cloneNode(true)
 
   const compDiv = document.getElementById(computer)
   const compClone = compDiv.cloneNode(true)
 
+  toggleAttack(playerClone, compClone)
   const playerAndCompDiv = document.createElement('div')
   playerAndCompDiv.appendChild(playerClone)
   playerAndCompDiv.appendChild(compClone)
-  playerAndCompDiv.id = 'someId'
+
+  playerAndCompDiv.id = 'pokeData'
   document.body.appendChild(playerAndCompDiv)
 }
+// show attack buttons when pokemon have been selected
+function toggleAttack(playerCloneDiv, compCloneDiv) {
+  // creating buttons below
+  let playerAttack = document.createElement('button')
+  let computerAttack = document.createElement('button')
+
+  // append button move based on playerDiv
+  switch (playerCloneDiv.id) {
+    case 'Cyndaquil':
+      playerAttack.innerHTML = Cyndaquil.move
+      playerAttack.id = Cyndaquil.move
+      playerCloneDiv.appendChild(playerAttack)
+      break
+    case 'Totodile':
+      playerAttack.innerHTML = Totodile.move
+      playerAttack.id = Totodile.move
+      playerCloneDiv.appendChild(playerAttack)
+      break
+    case 'Chikorita':
+      playerAttack.innerHTML = Chikorita.move
+      playerAttack.id = Chikorita.move
+      playerCloneDiv.appendChild(playerAttack)
+      break
+    default:
+      break
+  }
+  // append button move based on computerDiv
+  switch (compCloneDiv.id) {
+    case 'Cyndaquil':
+      computerAttack.innerHTML = Cyndaquil.move
+      computerAttack.id = Cyndaquil.move
+      compCloneDiv.appendChild(computerAttack)
+      break
+    case 'Totodile':
+      computerAttack.innerHTML = Totodile.move
+      computerAttack.id = Totodile.move
+      compCloneDiv.appendChild(computerAttack)
+      break
+    case 'Chikorita':
+      computerAttack.innerHTML = Chikorita.move
+      computerAttack.id = Chikorita.move
+      compCloneDiv.appendChild(computerAttack)
+      break
+    default:
+      break
+  }
+}
+
 // restart game button
 function restartGame() {
   window.location.reload(false)
